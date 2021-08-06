65664ad025840ec73059a1271e081ca214fbab2d
Brando Collins <ghost.rkrv3@gmail.com>
2021-08-06 09:48:54 -0500

    Completion of Phase One

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3> My First React </h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          React is a elite front-end library. There are hundreds of applications. Good practice makes perfect.
+          </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
